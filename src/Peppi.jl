b27f2b1b54d7d891fdd7cac16e47e2d8fd1ed93a
jph6366 <jphardee@gmail.com>
2026-03-21 22:27:36 -0400

    minor updates

--- src/Peppi.jl
+++ src/Peppi.jl
@@ -5,6 +5,25 @@ include("parse.jl")
 
 using .PeppiJlrs: read_slippi as _read_slippi, read_peppi as _read_peppi, get_start, get_end, get_metadata, get_frames_arrow_path
 import JSON
+using Arrow
+
+# Parse frames from Arrow struct array (column-oriented)
+function frames_from_sa(arrow_frames)
+
+    port_arrays = getproperty.(arrow_frames, :ports)
+    # TODO Is Follower	bool	Value is 1 for Nana and 0 otherwise... P3 ?
+    portenums = length(first(port_arrays)) != 2 ? (:P1, :P2, :P3, :P4) : (:P1, :P2)
+    ports = map(portenums) do p
+        port = getproperty.(port_arrays, p)
+        # TODO write logic for `is_follower`
+        # Value is 1 for Nana and 0 otherwise
+        # follower = nothing
+        leader = getproperty.(port, :leader)
+        (pre = getproperty.(leader, :pre), post = getproperty.(leader, :post))
+    end
+
+    (id = getproperty.(arrow_frames, :id), ports = ports)
+end
 
 """
     read_slippi(path::String; skip_frames::Bool=false) -> Game
@@ -88,5 +107,5 @@ function read_peppi(path::String; skip_frames::Bool=false)
     )
 end
 
-export read_slippi, read_peppi, Frame, PortData, Data, Pre, Post
+export read_slippi, read_peppi
 end
